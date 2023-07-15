8caa68408351160b04778d6f26a401da62d4940e
sachin <sachin14051@gmail.com>
2023-07-16 00:04:16 +0530

    working on class

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 var cl = console.log;
+function a(b, c) {
+    return b + c;
+}
+cl(a(10, 50));
+cl(typeof a);
 var Person = /** @class */ (function () {
-    function Person(fn, ln, num, role) {
-        this.fname = fn;
-        this.lname = ln;
-        this.adhar_no = num;
-        this.role = ["c"];
+    function Person(fn, ln, ad, p) {
+        this.fname = fn,
+            this.lname = ln,
+            this.adhar = ad,
+            this.place = p;
     }
+    Person.prototype.describe = function () {
+        //this.adhar =789 
+        this.fname = "sss";
+    };
     return Person;
 }());
-var admin = new Person;
+var user = new Person("sachin", "shelke", 123456, ["nanded", "pune"]);
+var user1 = new Person("sachin", "doe", 123456, ["nanded", "pune"]);
+var user2 = new Person("sachin", "doe", 78, ["nanded", "pune"]);
+var user3 = new Person("sachin", "doe", 7896, ["nanded", "pune"]);
+cl(user.fname);
+cl(user.fname = "jhon");
+cl(user);
+// public is used to reasign and globally show 
+//cl(user.lname) //Property 'lname' is private and only accessible within class 'Person'.
+cl(user1);
--- app.ts
+++ app.ts
@@ -1,19 +1,46 @@
 let cl = console.log;
 
-class Person {
- public   fname : string;
-    lname : string;
-    adhar_no : number;
-    role : Array <string>;
-    constructor (fn:string,ln:string,num:number,role:string[]){
-         this.fname = fn;
-        this.lname = ln;
-        this.adhar_no = num;
-        this.role = ["c"]
 
-
-    }
-
+function a (b:number,c:number): number{
+    return b + c
 }
 
-let admin = new  Person
+cl(a(10,50))
+cl(typeof a)
+
+
+class Person {
+  public  fname : String;
+   private lname :string;
+   private readonly adhar : number;
+    place : string[];
+
+    constructor(fn:string,ln:string,ad:number,p:string[]){
+        this.fname = fn,
+        this.lname = ln,
+        this.adhar = ad,
+        this.place = p
+    }
+    describe(){
+        //this.adhar =789 
+        this.fname = "sss"
+    }    
+}
+
+let user = new Person ("sachin","shelke",123456,["nanded","pune"]);
+let user1 = new Person ("sachin","doe",123456,["nanded","pune"]);
+let user2 = new Person ("sachin","doe",78,["nanded","pune"]);
+let user3 = new Person ("sachin","doe",7896,["nanded","pune"]);
+
+
+
+
+cl(user.fname)
+cl(user.fname="jhon")
+
+cl(user)
+// public is used to reasign and globally show 
+
+//cl(user.lname) //Property 'lname' is private and only accessible within class 'Person'.
+cl(user1)
+
